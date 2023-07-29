db986a41d127aa29a24cc4a81120b1f8dae655bf
Alejandro Trujillo <alejandro.trujillo.gon@gmail.com>
2023-07-29 10:50:06 +0200

    Sistema de audio

---
+++ Assets/Scripts/Audio/AudioPlayer.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour
+{
+    [Serializable]
+    public class AudioClipData
+    {
+        public AudioClip Clip;
+        public float Volume;
+    }
+
+    public static AudioPlayer Instance;
+
+    [SerializeField]
+    private AudioSource audioSource;
+
+    [SerializeField]
+    private List<AudioClipData> musicClips;
+    [SerializeField]
+    private List<AudioClipData> sfxClips;
+
+    private void Awake()
+    {
+
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            //Destroy(gameObject);
+        }
+    }
+
+    public void PlaySFX(string name)
+    {
+        var clip = sfxClips.Find(x => x.Clip.name == name);
+        audioSource.PlayOneShot(clip.Clip, clip.Volume);
+    }
+
+    public void PlayMusic(string name)
+    {
+        var clip = musicClips.Find(x => x.Clip.name == name);
+        audioSource.clip = clip.Clip;
+        audioSource.volume = clip.Volume;
+        audioSource.loop = true;
+        audioSource.Play();
+    }
+
+    public void SetMute(bool isMuted)
+    {
+        audioSource.mute = isMuted;
+    }
+
+    public void StopSfx()
+    {
+        audioSource.Stop();
+    }
+}
+
