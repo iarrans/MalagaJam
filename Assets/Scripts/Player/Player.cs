fe8f80e96ebaccba0f25ea1d9810d9a64522612a
Alejandro Trujillo <alejandro.trujillo.gon@gmail.com>
2023-07-29 16:23:54 +0200

    Win/Lose condition y paneles placeholders

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -10,17 +10,27 @@ public class Player : MonoBehaviour
     public TextMeshProUGUI textoCordura;
     public TextMeshProUGUI textoVisualizations;
 
+    [SerializeField]
+    int visualizationsForWin;
 
     public void IncreaseVisualizations(int amount)
     {
         visualizations += amount;
         textoVisualizations.text = visualizations.ToString();
+        if (visualizations > visualizationsForWin)
+        {
+            WinGame();
+        }
     }
 
     public void DecreaseVisualizations(int amount)
     {
         visualizations -= amount;
         textoVisualizations.text = visualizations.ToString();
+        if (visualizations < 0)
+        {
+            LoseGame();
+        }
     }
 
     public void IncreaseSanity(int amount)
@@ -33,5 +43,23 @@ public class Player : MonoBehaviour
     {
         sanity -= amount;
         textoCordura.text = sanity.ToString();
+        if (sanity <= 0)
+        {
+            LoseGame();
+        }
+    }
+
+    void WinGame()
+    {
+        //No se si aqui va cinematica o que
+        //Por ahora panel de victoria placeholder
+        GameObject winPanel = GameObject.FindGameObjectWithTag("WinPanel");
+        winPanel.SetActive(true);
+    }
+
+    void LoseGame()
+    {
+        GameObject losePanel = GameObject.FindGameObjectWithTag("LosePanel");
+        losePanel.SetActive(true);
     }
 }
